01ed938b061da55f37a0ac89093cdffd708a2c6e
Xiangyu Guo <tobielf@gmail.com>
2017-02-26 09:37:41 -0700

    Commit example of Testing Primality.
    Traditional way: find factor from 1 to ⎷n.
    Fermat’s Little Theorem -> Fermat Test -> Probabilistic methods.

--- section1-2.scm
+++ section1-2.scm
@@ -194,4 +194,36 @@
 ;;; 4. gcd 2 0 (r 4 2)
 ;;; and 4 times in applicative-order evaluation
 
+;;; Example Testing for Primality
+(define (smallest-divisor n)
+  (find-divisor n 2))
 
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+;;; Fermat test(Fermat's Little Theorem)
+;;; Will be fooled by Carmichael numbers
+;;; e.g, 561, 1105, 1729, 2465, 2821, and 6601.
+;;; There are 255 Carmichael numbers out of 100,000,000.(very rare!)
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp) (remainder (square (expmod base (/ exp 2) m)) m))
+        (else (remainder (* base (expmod base (- exp 1) m)) m))))
+
+(define (fermat-test n)
+  (define (try-it a)
+    (= (expmod a n n) a))
+  (try-it (+ 1 (random (- n 1)))))
+
+(define (fast-prime? n times)
+  (cond ((= times 0) true)
+        ((fermat-test n) (fast-prime? n (- times 1)))
+        (else false)))
