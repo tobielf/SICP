42167f03673415214b0c212a700f5c454e00c31d
Xiangyu Guo <tobielf@gmail.com>
2017-02-27 21:42:19 -0700

    Add answer to Exercise 1.22

--- section1-2.scm
+++ section1-2.scm
@@ -236,4 +236,24 @@
 ;> (smallest-divisor 19999)
 ;Value: 7
 
+;;; Exercise 1.22
+(define (timed-prime-test n)
+  (newline)
+  (display n)
+  (start-prime-test n (runtime)))
+
+(define (start-prime-test n start-time)
+  (if (prime? n)
+    (report-prime (- (runtime) start-time))))
+
+(define (report-prime elapsed-time)
+  (display " *** ")
+  (display elapsed-time))
+
+(define (search-for-primes start end)
+  (if (even? start)
+      (search-for-primes (+ start 1) end)
+      (cond ((< start end) (timed-prime-test start)
+                           (search-for-primes (+ start 2) end)))))
+
 
