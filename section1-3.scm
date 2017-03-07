82938d42c72a8fdd28f411bccca42253f3903647
Xiangyu Guo <tobielf@gmail.com>
2017-03-06 23:10:28 -0700

    Add example of lambda and let.

--- section1-3.scm
+++ section1-3.scm
@@ -141,3 +141,42 @@
             null-value)))))
   (iter a null-value))
 
+;;; "lambda" expression and "let" syntax sugar
+;;; previous example without helper function
+(define (pi-sum a b)
+  (sum (lambda (x) (+ x 4)) 
+       a
+       (lambda (x) (/ 1.0 (* x (+ x 2))))
+       b))
+
+(define (integral f a b dx)
+  (* (sum f
+          (+ a (/ dx 2.0))
+          (lambda (x) (+ x dx))
+          b)
+     dx))
+
+;;; with helper function.
+(define (f x y)
+  (define (f-helper a b)
+    (+ (* x (square a))
+       (* y b)
+       (* a b)))
+  (f-helper (+ 1 (* x y))
+            (- 1 y)))
+
+;;; with anonymous lambda expression
+(define (f x y)
+  ((lambda (a b) 
+     (+ (* x (square a))
+        (* y b)
+        (* a b)))
+  (+ 1 (* x y))
+  (- 1 y)))
+
+(define (f x y)
+  (let ((a (+ 1 (* x y)))
+        (b (- 1 y)))
+    (+ (* x (square a))
+       (* y b)
+       (* a b))))
