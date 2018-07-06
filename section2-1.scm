fc6953e405b59713312f5b3c814a89464c73dc10
Xiangyu Guo <tobielf@gmail.com>
2018-07-06 05:03:34 -0700

    Add answer to Exercise 2.7
    Assume the user give the lower bound then the upper bound.

--- section2-1.scm
+++ section2-1.scm
@@ -256,4 +256,32 @@
 ;Value: 8
 
 
+;;; Exercise 2.7
+(define (make-interval a b)
+  (cons a b))
+
+(define (lower-bound y)
+  (car x))
+
+(define (upper-bound x)
+  (cdr x))
+
+;;; Example Interval Arithmetic
+(define (add-interval x y)
+  (make-interval (+ (lower-bound x) (lower-bound y))
+                 (+ (upper-bound x) (upper-bound y))))
+
+(define (mul-interval x y)
+  (let ((p1 (* (lower-bound x) (lower-bound y)))
+        (p2 (* (lower-bound x) (upper-bound y)))
+        (p3 (* (upper-bound x) (lower-bound y)))
+        (p4 (* (upper-bound x) (upper-bound y))))
+    (make-interval (min p1 p2 p3 p4)
+                   (max p1 p2 p3 p4))))
+
+(define (div-interval x y)
+  (mul-interval x 
+                (make-interval (/ 1.0 (upper-bound y))
+                               (/ 1.0 (lower-bound y)))))
+
 
