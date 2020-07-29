b817c010f4f60ccfae82a6582af51260a414b409
Xiangyu Guo <tobielf@gmail.com>
2020-07-28 23:45:43 -0700

    Update section2-2.scm
    List operations.

--- section2-2.scm
+++ section2-2.scm
@@ -1,3 +1,10 @@
 (load "section2-1")
 
 (define one-through-four (list 1 2 3 4))
+
+(define (list-ref items n)
+  (if (= n 0)
+      (car items)
+      (list-ref (cdr items) (- n 1))))
+
+(define squares (list 1 4 9 16 25))
