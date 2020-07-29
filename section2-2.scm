5119b991f82244bfc07ace02a67e3a55ca8ef76c
Xiangyu Guo <tobielf@gmail.com>
2020-07-28 23:51:15 -0700

    Update section2-2.scm
    Length implementation using an accumulator.

--- section2-2.scm
+++ section2-2.scm
@@ -13,3 +13,13 @@
   (if (null? items)
       0
       (+ 1 (length (cdr items)))))
+
+(define odds (list 1 3 5 7))
+
+(define (length items)
+  (define (length-iter a count)
+    (if (null? a)
+        count
+        (length-iter (cdr a) (+ 1 count))))
+  (length-iter items 0)
+)
