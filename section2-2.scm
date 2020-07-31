1a821d84ecfc160e3a288d0133c700175c92fbd7
Xiangyu Guo <tobielf@gmail.com>
2020-07-31 00:49:31 -0700

    Add solution for Exercise 2.23
    Foreach implementation.

--- section2-2.scm
+++ section2-2.scm
@@ -152,3 +152,13 @@
               (append answer (list (square (car things)))
                     ))))
   (iter items (list)))
+
+;;; Exercise 2.23
+(define (for-each proc items)
+  (cond ((null? items) #t)
+        (else (proc (car items))
+              (for-each proc (cdr items)))))
+
+(for-each (lambda (x) (newline) (display x)) 
+          (list 57 321 88))
+
