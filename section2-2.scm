073674a4fa0dd3036ade651055ce8a4ca7bdd5ae
Xiangyu Guo <tobielf@gmail.com>
2020-08-02 11:35:58 -0700

    Update section2-2.scm
    Tree structure.

--- section2-2.scm
+++ section2-2.scm
@@ -162,3 +162,14 @@
 (for-each (lambda (x) (newline) (display x)) 
           (list 57 321 88))
 
+(define (count-leaves x)
+  (cond ((null? x) 0)
+        ((not (pair? x)) 1)
+        (else (+ (count-leaves (car x))
+                 (count-leaves (cdr x))))))
+
+
+
+
+
+
