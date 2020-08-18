f5fb02febc1ff3eb043ec7221b82d4a1cebe237b
Xiangyu Guo <tobielf@gmail.com>
2020-08-18 13:54:43 -0700

    Add solution to Ex 2.30
    Square tree with two methods. (Directly and map + recursion)

--- section2-2.scm
+++ section2-2.scm
@@ -306,4 +306,29 @@
 
 (scale-tree (list 1 (list 2 (list 3 4) 5) (list 6 7)) 10)
 
+;;; Exercise 2.30
+
+(define (square-tree tree)
+  (cond ((null? tree) `())
+        ((not (pair? tree)) (square tree))
+        (else (cons (square-tree (car tree)) 
+                    (square-tree (cdr tree))))))
+
+(square-tree 
+   (list 1 
+         (list 2 
+         (list 3 4) 5)
+         (list 6 7)))
+
+(define (square-tree tree)
+  (map (lambda (sub-tree) (if (pair? sub-tree)
+      (square-tree sub-tree)
+      (square sub-tree))) tree))
+
+(square-tree 
+   (list 1 
+         (list 2 
+         (list 3 4) 5)
+         (list 6 7)))
+
 
