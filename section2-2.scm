ab520364d98e7ea8c9dbc20b98d26f09c71f4fe3
Xiangyu Guo <tobielf@gmail.com>
2020-08-19 10:52:39 -0700

    Modular design.
    Designs that are constructed by combining relatively independent pieces, and reuse pieces.

--- section2-2.scm
+++ section2-2.scm
@@ -356,3 +356,72 @@
       (let ((rest (subsets (cdr s))))
         (append rest (map (lambda (sub-set) (cons (car s) sub-set)) rest)))))
 
+(define (sum-odd-squares tree)
+  (cond ((null? tree) 0)
+        ((not (pair? tree)) 
+         (if (odd? tree)
+             (square tree)
+             0))
+        (else (+ (sum-odd-squares (car tree)) 
+                 (sum-odd-squares (cdr tree))))))
+
+(define (even-fibs n)
+  (define (next k)
+    (if (> k n)
+        `()
+        (let ((f (fib k)))
+            (if (even? f)
+                (cons f (next (+ k 1)))
+                (next (+ k 1))))))
+  (next 0))
+
+(define (filter predicate sequence)
+  (cond ((null? sequence) `())
+        ((predicate (car sequence))
+         (cons (car sequence) (filter predicate (cdr sequence))))
+        (else (filter predicate (cdr sequence)))))
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (enumerate-interval low high)
+  (if (> low high)
+      `()
+      (cons low (enumerate-interval (+ low 1) high))))
+
+; fringe from Ex 2.28
+
+(define (enumerate-tree tree)
+  (cond ((null? tree) `())
+        ((not (pair? tree)) (list tree))
+        (else (append (enumerate-tree (car tree)) 
+                      (enumerate-tree (cdr tree))))))
+
+(define (sum-odd-squares tree)
+  (accumulate +
+              0
+              (map square (filter odd?
+                                  (enumerate-tree tree)))))
+
+(define (even-fibs n)
+  (accumulate cons
+              `()
+              (filter even?
+                      (map fib 
+                           (enumerate-interval 0 n)))))
+
+(define (list-fib-squares n)
+  (accumulate cons
+              `()
+              (map square
+                   (map fib
+                        (enumerate-interval 0 n)))))
+
+(define (product-of-squares-of-odd-elements sequence)
+  (accumulate *
+              1
+              (map square
+                   (filter odd? sequence))))
