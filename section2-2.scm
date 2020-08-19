afd550292442f89b7c43b4bd5cc607b1e40d73bf
Xiangyu Guo <tobielf@gmail.com>
2020-08-19 11:33:55 -0700

    Add answer to Exercise 2.33

--- section2-2.scm
+++ section2-2.scm
@@ -425,3 +425,14 @@
               1
               (map square
                    (filter odd? sequence))))
+
+;;; Exercise 2.33
+(define (map p sequence)
+  (accumulate (lambda (x y) (cons (p x) y)) `() sequence))
+
+(define (append seq1 seq2)
+  (accumulate cons seq2 seq1))
+
+(define (length sequence)
+  (accumulate (lambda (x y) (+ y 1)) 0 sequence))
+
