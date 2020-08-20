abe7c1c9d1627ba4397819b672b0ca6563d240fb
Xiangyu Guo <tobielf@gmail.com>
2020-08-20 14:11:33 -0700

    Nested mapping.
    flatmap, and prime-sum-pairs.

--- section2-2.scm
+++ section2-2.scm
@@ -487,3 +487,22 @@
 
 (define (reverse sequence)
   (fold-left (lambda (x y) (cons y x)) `() sequence))
+
+(define (flatmap proc seq)
+  (accumulate append `() (map proc seq)))
+
+(define (prime-sum? pair)
+  (prime? (+ (car pair) (cadr pair))))
+
+(define (make-pair-sum pair)
+  (list (car pair) (cadr pair) (+ (car pair) (cadr pair))))
+
+(define (prime-sum-pairs n)
+  (map make-pair-sum
+       (filter prime-sum?
+               (flatmap
+                (lambda (i)
+                  (map (lambda (j) (list i j))
+                       (enumerate-interval 1 (- i 1))))
+                (enumerate-interval 1 n)))))
+
