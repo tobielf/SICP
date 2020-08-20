cae3ae998e60975ae3e90fe7c930349429f64d19
Xiangyu Guo <tobielf@gmail.com>
2020-08-20 16:59:10 -0700

    Add answer to Exercise 2.40

--- section2-2.scm
+++ section2-2.scm
@@ -518,3 +518,14 @@
   (filter (lambda (x) (not (= x item)))
           sequence))
 
+;;; Exercise 2.40
+(define (unique-pairs n)
+  (flatmap (lambda (i) 
+             (map (lambda (j) (list i j)) 
+                  (enumerate-interval 1 (- i 1)))) 
+           (enumerate-interval 2 n)))
+
+(define (prime-sum-pairs n)
+  (map make-pair-sum
+       (filter prime-sum?
+               (unique-pairs n))))
