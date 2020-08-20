f428c90846cb602666a2497fc5e84978f62cbc4c
Xiangyu Guo <tobielf@gmail.com>
2020-08-20 14:23:35 -0700

    Remove set, permutation set.

--- section2-2.scm
+++ section2-2.scm
@@ -506,3 +506,15 @@
                        (enumerate-interval 1 (- i 1))))
                 (enumerate-interval 1 n)))))
 
+(define (permutations s)
+  (if (null? s)
+      (list `())
+      (flatmap (lambda (x)
+                  (map (lambda (p) (cons x p))
+                       (permutations (remove x s))))
+               s)))
+
+(define (remove item sequence)
+  (filter (lambda (x) (not (= x item)))
+          sequence))
+
