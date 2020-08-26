5b0ac2bc68cf0686ef98ce54d544147915d07f63
Xiangyu Guo <tobielf@gmail.com>
2020-08-25 17:44:45 -0700

    Add solution to Exercise 2.47.
    Data abstraction about frame.

--- section2-2.scm
+++ section2-2.scm
@@ -674,3 +674,27 @@
   (make-vect (* (xcor-vect v) s) 
              (* (ycor-vect v) s)))
 
+;;; Exercise 2.47
+(define (make-frame origin edge1 edge2)
+  (list origin edge1 edge2))
+
+(define (origin-frame frame)
+  (car frame))
+
+(define (edge1-frame frame)
+  (cadr frame))
+
+(define (edge2-frame frame)
+  (cadr (cdr frame)))
+
+(define (make-frame origin edge1 edge2)
+  (cons origin (cons edge1 edge2)))
+
+(define (origin-frame frame)
+  (car frame))
+
+(define (edge1-frame frame)
+  (cadr frame))
+
+(define (edge2-frame frame)
+  (cdr (cdr frame)))
