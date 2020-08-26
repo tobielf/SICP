7d73bbfcc3e119ca1c4511b90ad8d03103f54574
Xiangyu Guo <tobielf@gmail.com>
2020-08-25 18:19:53 -0700

    A simple painter.
    Use draw-line and foreach to print segment-list.

--- section2-2.scm
+++ section2-2.scm
@@ -698,3 +698,13 @@
 
 (define (edge2-frame frame)
   (cdr (cdr frame)))
+
+(define (segments->painter segment-list)
+  (lambda (frame) 
+    (for-each
+      (lambda (segment)
+        (draw-line
+          ((frame-coord-map frame) (start-segment segment))
+          ((frame-coord-map frame) (end-segment segment))))
+    segment-list)))
+
