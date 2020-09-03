6519a6c16043654ccfce0e1fe13f5fd68a3e0aee
Xiangyu Guo <tobielf@gmail.com>
2020-09-02 23:23:03 -0700

    Add solution to Exercise 2.56
    Deriv on exponetiation.

--- section2-3.scm
+++ section2-3.scm
@@ -102,3 +102,36 @@
         ((=number? m2 1) m1)
         ((and (number? m1) (number? m2)) (* m1 m2))
         (else (list '* m1 m2))))
+
+;;; Exercise 2.56
+(define (make-exponentiation b exp)
+  (cond ((=number? exp 0) 1)
+        ((=number? exp 1) b)
+        (else (list '** b exp))))
+
+(define (exponentiation? x)
+  (and (pair? x) (eq? (car x) '**)))
+
+(define (base e) (cadr e))
+
+(define (exponent e) (caddr e))
+
+(define (deriv exp var)
+  (cond ((number? exp) 0)
+        ((variable? exp) 
+         (if (same-variable? exp var) 1 0))
+        ((sum? exp)
+         (make-sum (deriv (addend exp) var)
+                   (deriv (augend exp) var)))
+        ((product? exp)
+         (make-sum 
+           (make-product (multiplier exp)
+                         (deriv (multiplicand exp) var))
+           (make-product (deriv (multiplier exp) var)
+                         (multiplicand exp))))
+        ((exponentiation? exp)
+         (make-product (exponent exp)
+                       (make-product (make-exponentiation (base exp) (- (exponent exp) 1))
+                                     (deriv (base exp) var))))
+        (else (error "unknown expression type -- DERIV" exp))))
+
