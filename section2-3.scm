f19f3ead7b860e74ee741ce3a9886f02aaf1f1f7
Xiangyu Guo <tobielf@gmail.com>
2020-09-03 22:38:00 -0700

    Add solution to Exercise 2.59
    Union-set operation.

--- section2-3.scm
+++ section2-3.scm
@@ -215,3 +215,11 @@
   (cond ((or (null? set1) (null? set2)) `())
         ((element-of-set? (car set1) set2) (cons (car set1) (intersection-set (cdr set1) set2)))
         (else (intersection-set (cdr set1) set2))))
+
+;;; Exercise 2.59
+; O(n^2) complexity.
+(define (union-set set1 set2)
+  (cond ((null? set1) set2)
+        ((null? set2) set1)
+        ((element-of-set? (car set1) set2) (union-set (cdr set1) set2))
+        (else (cons (car set1) (union-set (cdr set1) set2)))))
