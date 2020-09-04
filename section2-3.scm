eb2da2ee396ee85d4e3ea5a763a8b0703d4b640e
Xiangyu Guo <tobielf@gmail.com>
2020-09-04 12:21:44 -0700

    Add solution to Exercise 2.64.

--- section2-3.scm
+++ section2-3.scm
@@ -401,3 +401,37 @@
 ; No, 
 ; tree->list-1 T(n) = 2 * T(n/2) + O(n) ==> O(n log n)
 ; tree->list-2 T(n) = 2 * T(n/2) + O(1) ==> O(n)
+
+;;; Exercise 2.64
+(define (list->tree elements)
+  (car (partial-tree elements (length elements))))
+
+(define (partial-tree elts n)
+  (if (= n 0)
+      (cons `() elts)
+      (let ((left-size (quotient (- n 1) 2)))
+        (let ((left-result (partial-tree elts left-size)))
+          (let ((left-tree (car left-result))
+                (non-left-elts (cdr left-result))
+                (right-size (- n (+ left-size 1))))
+            (let ((this-entry (car non-left-elts))
+                  (right-result (partial-tree (cdr non-left-elts) right-size)))
+              (let ((right-tree (car right-result))
+                    (remaining-elts (cdr right-result)))
+                (cons (make-tree this-entry left-tree right-tree)
+                      remaining-elts))))))))
+
+; a. how partial-tree works? Draw the tree produced by list->tree for the list (1 3 5 7 9 11)
+;              5
+;            /   \
+;           1     9
+;            \   / \
+;             3 7  11
+(list->tree '(1 3 5 7 9 11))
+;Value: (5 (1 () (3 () ())) (9 (7 () ()) (11 () ())))
+; partial-tree divide the list to two halves and one this-entry list[(n - 1)/2]
+; recursively construct the left-tree and right-tree by consuming the elements.
+; Eventually, the elements will be an empty list.
+
+; b. Time complexity?
+; T(n) = 2 * T(n/2) + O(1) ==> O(n)
