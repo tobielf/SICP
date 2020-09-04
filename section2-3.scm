a9db80005213c8f9447be7cb9d394659ed08377d
Xiangyu Guo <tobielf@gmail.com>
2020-09-03 18:10:44 -0700

    Add answer to Exercise 2.58 a.

--- section2-3.scm
+++ section2-3.scm
@@ -165,3 +165,34 @@
 
 (deriv '(* x y (+ x 3)) 'x)
 (deriv (make-arbitrary-product 'x 'y (make-arbitrary-sum 'x 3)) 'x)
+
+;;; Exercise 2.58
+;a
+(define (make-sum a1 a2)
+  (cond ((=number? a1 0) a2)
+        ((=number? a2 0) a1)
+        ((and (number? a1) (number? a2)) (+ a1 a2))
+        (else (list a1 '+ a2))))
+
+(define (make-product m1 m2)
+  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
+        ((=number? m1 1) m2)
+        ((=number? m2 1) m1)
+        ((and (number? m1) (number? m2)) (* m1 m2))
+        (else (list m1 '* m2))))
+
+(define (sum? x)
+  (and (pair? x) (eq? (cadr x) '+)))
+
+(define (addend s) (car s))
+
+(define (augend s) (caddr s))
+
+(define (product? x)
+  (and (pair? x) (eq? (cadr x) '*)))
+
+(define (multiplier p) (car p))
+
+(define (multiplicand p) (caddr p))
+
+;b is harder, so I decided to do later.
