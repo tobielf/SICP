1e9b7fecc971ff137624a74ff1c31f7b400c7c3c
Xiangyu Guo <tobielf@gmail.com>
2020-09-03 22:47:05 -0700

    Add solution to Exercise 2.60
    Allowing duplicates in the set.

--- section2-3.scm
+++ section2-3.scm
@@ -223,3 +223,22 @@
         ((null? set2) set1)
         ((element-of-set? (car set1) set2) (union-set (cdr set1) set2))
         (else (cons (car set1) (union-set (cdr set1) set2)))))
+
+;;; Exercise 2.60 Allow duplicates.
+; O(n) complexity.
+(define (element-of-set? x set)
+  (cond ((null? set) #f)
+        ((equal? x (car set)) #t)
+        (else (element-of-set? x (cdr set)))))
+
+; O(1) complexity.
+(define (adjoin-set x set) (cons x set))
+
+; O(n^2) complexity.
+(define (intersection-set set1 set2)
+  (cond ((or (null? set1) (null? set2)) `())
+        ((element-of-set? (car set1) set2) (cons (car set1) (intersection-set (cdr set1) set2)))
+        (else (intersection-set (cdr set1) set2))))
+
+; O(n) complexity.
+(define (union-set set1 set2) (append set1 set2))
