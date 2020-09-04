e8720a1adacf48f4bfd77bb18d771042cdd0fd48
Xiangyu Guo <tobielf@gmail.com>
2020-09-04 11:47:23 -0700

    Add solution to Exercise 2.63
    Recurrence relation analysis, divide and conquer technique. Master theorem, refer to CLRS.

--- section2-3.scm
+++ section2-3.scm
@@ -346,3 +346,58 @@
                      (left-branch set)
                      (adjoin-set x (right-branch set))))))
 
+;;; Exercise 2.63
+(define (tree->list-1 tree)
+  (if (null? tree)
+      `()
+      (append (tree->list-1 (left-branch tree))
+              (cons (entry tree) (tree->list-1 (right-branch tree))))))
+
+(define (tree->list-2 tree)
+  (define (copy-to-list tree result-list)
+    (if (null? tree)
+        result-list
+        (copy-to-list (left-branch tree)
+                      (cons (entry tree)
+                            (copy-to-list (right-branch tree)
+                            result-list)))))
+  (copy-to-list tree `()))
+
+; a. Do the two procudures produce the same result for every tree?
+; Yes, they are both in-order traverse of the tree.
+(define tree1 (make-tree 7 (make-tree 3 (make-tree 1 '() '())
+                                        (make-tree 5 '() '())) 
+                           (make-tree 9 '() 
+                                        (make-tree 11 '() '()))))
+
+(define tree2 (make-tree 3 (make-tree 1 '() '())
+                           (make-tree 7 (make-tree 5 '() '())
+                                        (make-tree 9 '() (make-tree 11 '() '())))))
+
+(define tree3 (make-tree 5 (make-tree 3 (make-tree 1 '() '())
+                                        '())
+                           (make-tree 9 (make-tree 7 '() '())
+                                        (make-tree 11 '() '()))))
+
+(tree->list-1 tree1)
+;Value: (1 3 5 7 9 11)
+
+(tree->list-2 tree1)
+;Value: (1 3 5 7 9 11)
+
+(tree->list-1 tree2)
+;Value: (1 3 5 7 9 11)
+
+(tree->list-2 tree2)
+;Value: (1 3 5 7 9 11)
+
+(tree->list-1 tree3)
+;Value: (1 3 5 7 9 11)
+
+(tree->list-2 tree3)
+;Value: (1 3 5 7 9 11)
+
+; b. Do the two procedures have the same order of growth?
+; No, 
+; tree->list-1 T(n) = 2 * T(n/2) + O(n) ==> O(n log n)
+; tree->list-2 T(n) = 2 * T(n/2) + O(1) ==> O(n)
