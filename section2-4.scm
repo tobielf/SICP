b06d10d3cee699188ee9863412ede41c57f0471a
Xiangyu Guo <tobielf@gmail.com>
2020-09-06 14:25:12 -0700

    A set of generic operations to abstract out the details.
    The system has been decomposed into three relatively independent parts. The abstract constructor/selector interface allowing two different underlying representations for high-level complex operation.

--- section2-4.scm
+++ section2-4.scm
@@ -15,28 +15,30 @@
                      (- (angle z1) (angle z2))))
 
 ; A representation easier for add/sub-complex, convert to magnitude+angle for mul/div-complex
-(define (real-part z) (car z))
-(define (imag-part z) (cdr z))
-(define (magnitude z)
+(define (real-part-rectangular z) (car z))
+(define (imag-part-rectangular z) (cdr z))
+(define (magnitude-rectangular z)
   (sqrt (+ (square (real-part z)) (square (imag-part z)))))
-(define (angle z)
+(define (angle-rectangular z)
   (atan (imag-part z) (real-part z)))
-(define (make-from-real-imag x y)
-  (cons x y))
-(define (make-from-mag-ang r a)
-  (cons (* r (cos a)) (* r (sin a))))
+(define (make-from-real-imag-rectangular x y)
+  (attach-tag 'rectangular (cons x y)))
+(define (make-from-mag-ang-rectangular r a)
+  (attach-tag 'rectangular
+              (cons (* r (cos a)) (* r (sin a)))))
 
 ; A representation easier for mul/div-complex, convert to real+imagine for add/sub-complex
-(define (real-part z)
+(define (real-part-polar z)
   (* (magnitude z) (cos (angle z))))
-(define (imag-part z)
+(define (imag-part-polar z)
   (* (magnitude z) (sin (angle z))))
-(define (magnitude z) (car z))
-(define (angle z) (cdr z))
-(define (make-from-real-imag x y)
-  (cons (sqrt (+ (square x) (square y)))))
-(define (make-from-mag-ang r a)
-  (cons r a))
+(define (magnitude-polar z) (car z))
+(define (angle-polar z) (cdr z))
+(define (make-from-real-imag-polar x y)
+  (attach-tag 'polar
+              (cons (sqrt (+ (square x) (square y))))))
+(define (make-from-mag-ang-polar r a)
+  (attach-tag 'polar (cons r a)))
 
 (define (attach-tag type-tag contents)
   (cons type-tag contents))
@@ -53,3 +55,24 @@
   (eq? (type-tag z) 'rectangular))
 (define (polar? z)
   (eq? (type-tag z) 'polar))
+
+(define (real-part z)
+  (cond ((rectangular? z) 
+         (real-part-rectangular (contents z)))
+        ((polar? z)
+         (real-part-polar (contents z)))
+        (else (error "Unknown type -- REAL-PART" z))))
+
+(define (imag-part z)
+  (cond ((rectangular? z) 
+         (imag-part-rectangular (contents z)))
+        ((polar? z)
+         (imag-part-polar (contents z)))
+        (else (error "Unknown type -- IMAG-PART" z))))
+
+(define (make-from-real-imag x y)
+  (make-from-real-imag-rectangular x y))
+
+(define (make-from-mag-ang r a)
+  (make-from-mag-ang-polar r a))
+
