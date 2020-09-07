0ca37789d1d6b0c0f32a1529a4f7261ad4c2799c
Xiangyu Guo <tobielf@gmail.com>
2020-09-06 21:34:33 -0700

    Add solution to Exercise 2.75

--- section2-4.scm
+++ section2-4.scm
@@ -303,3 +303,16 @@
           (else
             (error "Unknown op -- MAKE-FROM-REAL-IMAG" op))))
   dispatch)
+
+;;; Exercise 2.75
+(define (make-from-mag-ang r a)
+  (define (dispatch op)
+    (cond ((eq? op 'real-part) 
+           (* r (cos a)))
+          ((eq? op 'imag-part)
+           (* r (sin a)))
+          ((eq? op 'magnitude) r)
+          ((eq? op 'angle) a)
+          (else
+            (error "Unknown op -- MAKE-FROM-MAG-ANG" op))))
+  dispatch)
