b8dc1566fb5c5a460e222adb598c106393acde97
Xiangyu Guo <tobielf@gmail.com>
2020-09-06 21:30:46 -0700

    Message passing

--- section2-4.scm
+++ section2-4.scm
@@ -293,3 +293,13 @@
 ;   operations with new data-tag to the table.
 ;   We don't need to change the central system.
 
+(define (make-from-real-imag x y)
+  (define (dispatch op)
+    (cond ((eq? op 'real-part) x)
+          ((eq? op 'imag-part) y)
+          ((eq? op 'magnitude)
+           (sqrt (+ (square x) (square y))))
+          ((eq? op 'angle) (atan y x))
+          (else
+            (error "Unknown op -- MAKE-FROM-REAL-IMAG" op))))
+  dispatch)
