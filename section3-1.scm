6b616391c718f9749ec6741c0ff70ca47c61580c
Xiangyu Guo <tobielf@gmail.com>
2020-09-18 15:22:12 -0700

    Add answer to Exercise 3.2

--- section3-1.scm
+++ section3-1.scm
@@ -40,3 +40,13 @@
   (lambda (n)
     (set! sum (+ sum n))
     sum))
+
+;;; Exercise 3.2
+(define (make-monitored f)
+  (define counter 0)
+  (define (mf m)
+    (cond ((eq? m 'how-many-calls?) counter)
+          ((eq? m 'reset-count) (set! counter 0))
+          (else (begin (set! counter (+ counter 1))
+                       (f m)))))
+  mf)
