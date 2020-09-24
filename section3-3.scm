7bb2d94f48fcbae52b65730fc6dab72393fbd747
XiangyuGuo <tobielf@gmail.com>
2020-09-24 00:00:31 -0700

    Sharing and identity.
    Two structures looks the "same" but yields different result due to the underlying representations are different.

--- section3-3.scm
+++ section3-3.scm
@@ -85,3 +85,23 @@
 ;(d c b a)
 ;> v
 ;(a)
+
+; Sharing and identity.
+(define x (list 'a 'b))
+(define z1 (cons x x))
+
+(define z2 (cons (list 'a 'b) (list 'a 'b)))
+
+(define (set-to-wow! x)
+  (set-car! (car x) 'wow)
+  x)
+
+;> z1
+;((a b) a b)
+;> (set-to-wow! z1)
+;((wow b) wow b)
+;> z2
+;((a b) a b)
+;> (set-to-wow! z2)
+;((wow b) a b)
+;>
