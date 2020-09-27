75e4ccd979a6070e73fb2b9dbdb68c31bb56f71a
XiangyuGuo <tobielf@gmail.com>
2020-09-27 16:39:53 -0700

    Representing tables.

--- section3-3.scm
+++ section3-3.scm
@@ -415,3 +415,25 @@
 (define (rear-insert-deque! deque item) ((deque 'rear-insert-deque) item))
 (define (front-delete-deque! deque) ((deque 'front-delete-deque)))
 (define (rear-delete-deque! deque) ((deque 'rear-delete-deque)))
+
+(define (lookup key table)
+  (let ((record (assoc key (cdr table))))
+      (if record
+          (cdr record)
+          #f)))
+
+(define (assoc key records)
+  (cond ((null? records) #f)
+        ((equal? key (caar records)) (car records))
+        (else (assoc key (cdr records)))))
+
+(define (insert! key value table)
+  (let ((record (assoc key (cdr table))))
+      (if record
+          (set-cdr! record value)
+          (set-cdr! table
+                    (cons (cons key value) (cdr table)))))
+  'ok)
+
+(define (make-table)
+  (list '*table*))
